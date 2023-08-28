99949ad5a3b04155600ff6d2b3941e0456008bea
discoboy <jyjyartem@gmail.com>
2023-08-28 22:53:25 +0300

    fix: remove server caching

--- server/src/auth/router.py
+++ server/src/auth/router.py
@@ -3,7 +3,6 @@ import json
 from fastapi import Depends, APIRouter, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.ext.asyncio import AsyncSession
-from fastapi_cache.decorator import cache
 
 import src.database as _db
 import src.auth.schemas as _auth_schemas
